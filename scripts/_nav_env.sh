55647ed0aa30490fe6bdba94f54a93b9d27c26a1
Bziyue <954164207@qq.com>
2026-03-16 11:53:21 +0800

    修改启动环境硬编码

--- scripts/_nav_env.sh
+++ scripts/_nav_env.sh
@@ -4,19 +4,29 @@
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 ISAACLAB_DIR="${ISAACLAB_DIR:-/home/zdp/CodeField/IsaacLab-2.3.2}"
-ENV_NAME="${ENV_NAME:-env_isaacsim}"
-CONDA_ENV_DIR="${CONDA_ENV_DIR:-/home/zdp/anaconda3/envs/${ENV_NAME}}"
-ISAACSIM_DIR="${ISAACSIM_DIR:-/home/zdp/isaacsim}"
+ENV_NAME="${ENV_NAME:-${CONDA_DEFAULT_ENV:-env_isaacsim}}"
+
+if [[ -n "${CONDA_ENV_DIR:-}" ]]; then
+    CONDA_ENV_DIR="${CONDA_ENV_DIR}"
+elif [[ -n "${CONDA_PREFIX:-}" ]]; then
+    CONDA_ENV_DIR="${CONDA_PREFIX}"
+else
+    CONDA_ENV_DIR="/home/zdp/anaconda3/envs/${ENV_NAME}"
+fi
+
+if [[ -n "${ISAACSIM_DIR:-}" ]]; then
+    ISAACSIM_DIR="${ISAACSIM_DIR}"
+elif [[ -n "${ISAACSIM_PATH:-}" ]]; then
+    ISAACSIM_DIR="${ISAACSIM_PATH}"
+else
+    ISAACSIM_DIR=""
+fi
+
 RUNTIME_CACHE_DIR="${RUNTIME_CACHE_DIR:-${ROOT_DIR}/.runtime-cache}"
 
 check_nav_runtime_prereqs() {
     if [[ ! -x "${CONDA_ENV_DIR}/bin/python" ]]; then
         echo "Missing conda env python: ${CONDA_ENV_DIR}/bin/python" >&2
-        return 1
-    fi
-
-    if [[ ! -d "${ISAACSIM_DIR}" ]]; then
-        echo "Missing Isaac Sim directory: ${ISAACSIM_DIR}" >&2
         return 1
     fi
 }
@@ -32,13 +42,15 @@ setup_nav_runtime_env() {
     export WARP_CACHE_DIR="${RUNTIME_CACHE_DIR}/warp"
     export XDG_CACHE_HOME="${RUNTIME_CACHE_DIR}/xdg"
 
-    if [[ -f "${ISAACSIM_DIR}/setup_conda_env.sh" ]]; then
+    if [[ -n "${ISAACSIM_DIR}" && -f "${ISAACSIM_DIR}/setup_conda_env.sh" ]]; then
         set +u
         # shellcheck disable=SC1090
         source "${ISAACSIM_DIR}/setup_conda_env.sh"
         set -u
     fi
 
-    # Isaac Sim's setup script does not add omni.client on this machine.
-    export PYTHONPATH="${ISAACSIM_DIR}/kit/extscore/omni.client.lib${PYTHONPATH:+:${PYTHONPATH}}"
+    # If Isaac Sim is installed outside the conda env, add omni.client explicitly.
+    if [[ -n "${ISAACSIM_DIR}" && -d "${ISAACSIM_DIR}/kit/extscore/omni.client.lib" ]]; then
+        export PYTHONPATH="${ISAACSIM_DIR}/kit/extscore/omni.client.lib${PYTHONPATH:+:${PYTHONPATH}}"
+    fi
 }
