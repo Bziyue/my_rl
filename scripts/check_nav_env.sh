a860413bcc2f189755e49aa4c59cdd828fcc0560
Bziyue <954164207@qq.com>
2026-03-16 16:26:34 +0800

    修改检查命令

--- scripts/check_nav_env.sh
+++ scripts/check_nav_env.sh
@@ -8,6 +8,6 @@ source "${SCRIPT_DIR}/_nav_env.sh"
 
 setup_nav_runtime_env
 
-"${CONDA_ENV_DIR}/bin/python" -c "import omni.client; import isaaclab; import isaaclab_rl; from rsl_rl.modules import ActorCriticSRU; print('omni.client / isaaclab / isaaclab_rl / ActorCriticSRU import OK')"
+"${CONDA_ENV_DIR}/bin/python" -c "import omni.client; import isaaclab; import isaaclab_rl; from tensordict import TensorDict; from isaaclab_rl.rsl_rl import RslRlVecEnvWrapper; from rsl_rl.modules import ActorCriticSRU; print('omni.client / isaaclab / isaaclab_rl.rsl_rl / TensorDict / ActorCriticSRU import OK')"
 "${CONDA_ENV_DIR}/bin/python" "${ROOT_DIR}/sru-navigation-sim/scripts/train.py" --help >/dev/null
 echo "train.py entrypoint OK"
